0d5787e7b348379857a8f494140ebee36c8a8a7b
zhaichaoqun <81673615+1322688717@users.noreply.github.com>
2023-03-31 17:33:57 +0800

    add:增加test1的方法 第二次

--- app/src/main/java/com/example/studygit/MainActivity.kt
+++ app/src/main/java/com/example/studygit/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        test1()
+    }
+
+    private fun test1() {
+        TODO("Not yet implemented")
     }
 }
